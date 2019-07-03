dec98e170034ee53c8351b2e47acab26751bd2a9
Bertilla <sweetybertilla_francisxavier@cable.comcast.com>
2019-07-03 19:33:13 -0400

    Added face detection

---
+++ app/src/main/java/com/example/mlkitexample/FaceDetection.kt
@@ -0,0 +1,69 @@
+package com.example.mlkitexample
+
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import java.io.File
+import android.content.Intent
+import android.graphics.Bitmap
+import android.app.Activity
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions
+
+
+class FaceDetection : BaseActivity() {
+    private var currentPhotoFile: File? = null
+    private var imagePreview: ImageView? = null
+    private var textView: TextView? = null
+    private val CAMERA_REQUEST = 1888
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_still_image)
+        imagePreview = findViewById(R.id.image_preview)
+        textView = findViewById(R.id.result_text)
+        findViewById<ImageButton>(R.id.photo_camera_button)?.setOnClickListener { takePhoto() }
+    }
+
+    fun takePhoto() {
+        val cameraIntent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, CAMERA_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            val picture =
+                data?.extras?.get("data") as Bitmap
+                imagePreview?.setImageBitmap(picture)
+            val image = FirebaseVisionImage.fromBitmap(picture)
+            val options = FirebaseVisionFaceDetectorOptions.Builder()
+                .setClassificationMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
+                .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                .setMinFaceSize(0.15f)
+                .enableTracking()
+                .build()
+            val detector = FirebaseVision.getInstance()
+                .getVisionFaceDetector(options)
+
+            val result = detector.detectInImage(image)
+                .addOnSuccessListener { faces ->
+                    // Task completed successfully
+                    // ...
+                    if(faces.isEmpty()) {
+                        textView?.text = "No Face Detected"
+                    } else {
+                        textView?.text = "Face Detected"
+                    }
+                }
+                .addOnFailureListener {
+                    // Task failed with an exception
+                    // ...
+
+                }
+        }
+    }
+}
--- app/src/main/java/com/example/mlkitexample/MainActivity.kt
+++ app/src/main/java/com/example/mlkitexample/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.mlkitexample
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        face_detection?.setOnClickListener {
+            val intent = Intent(this, FaceDetection::class.java)
+            startActivity(intent)
+        }
+        }
 }
