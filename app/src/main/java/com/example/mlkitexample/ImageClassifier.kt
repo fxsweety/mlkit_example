0f666e018ebfdd9ea0883f73547836ba32317e01
Bertilla <sweetybertilla_francisxavier@cable.comcast.com>
2019-08-11 12:45:22 -0400

    few changes

--- app/src/main/java/com/example/mlkitexample/ImageClassifier.kt
+++ app/src/main/java/com/example/mlkitexample/ImageClassifier.kt
@@ -165,7 +165,7 @@ internal constructor(context: Context) {
     private const val LOCAL_MODEL_PATH = "automl/manifest.json"
 
     /** Name of the remote model in Firebase ML Kit server.  */
-    private const val REMOTE_MODEL_NAME = "fruits_mlexample"
+    private const val REMOTE_MODEL_NAME = "fruits_ml_example"
 
     /** Number of results to show in the UI.  */
     private const val RESULTS_TO_SHOW = 3
